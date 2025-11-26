2520f42e87719dd00207bcacc5091ab3ead6a699
Di Santo Andrea <andrea.disanto.08@marconigorgonzola.it>
2025-11-26 20:01:32 +0100

    creato la classe java e aggiungere un a cosa

---
+++ Esericizio_6_me/Veicolo.java
@@ -0,0 +1,15 @@
+package Esericizio_6_me;
+
+public class Veicolo {
+    private String marca;
+    private int velocitaMax;
+
+    public Veicolo(String marca,int velocitaMax){
+        this.marca = marca;
+        this.velocitaMax = velocitaMax;
+    }
+
+    
+
+
+}
