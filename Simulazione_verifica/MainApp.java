f51858000e39fd8ddb7f40748a5615bad833e695
Di Santo Andrea <andrea.disanto.08@marconigorgonzola.it>
2025-11-27 11:52:10 +0100

    aggiunto gki attributi

--- Simulazione_verifica/MainApp.java
+++ Simulazione_verifica/MainApp.java
@@ -1,5 +1,28 @@
 package Simulazione_verifica;
+import java.util.scanner;
+
 
 public class MainApp {
+
+     Articolo[] panini = new Articolo[3];
+     panini[0] = new Articolo("paninoclassico" , 3.30);
+     panini[1] = new Articolo("paninocotoletta" , 3.00);
+     panini[2] = new Articolo("paninowurstel" , 2.60);
+     
+     Articolo[] bibite = new Articolo[3];
+     bibite[0] = new Articolo("cocacola" , 3.30);
+     bibite[1] = new Articolo("estathe" , 3.00);
+     bibite[2] = new Articolo("cocacola-zero" , 2.60);
+
+     
+
+
+
+
+    public static void main(String[] args) {
+
+
+        
+    }
     
 }
