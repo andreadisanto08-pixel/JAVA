666492dbe2fed274d18b0d2e08b71f76f5107990
Di Santo Andrea <andrea.disanto.08@marconigorgonzola.it>
2025-11-27 13:31:20 +0100

    cambiato la S

--- Simulazione_verifica/MainApp.java
+++ Simulazione_verifica/MainApp.java
@@ -1,30 +1,52 @@
 package Simulazione_verifica;
-import java.util.scanner;
+import java.util.Scanner;
 
 
 public class MainApp {
-
-    public MainApp(){
-
-     Articolo[] panini = new Articolo[3];
-     panini[0] = new Articolo("paninoclassico" , 3.30);
-     panini[1] = new Articolo("paninocotoletta" , 3.00);
-     panini[2] = new Articolo("paninowurstel" , 2.60);
-     
-     Articolo[] bibite = new Articolo[3];
-     bibite[0] = new Articolo("cocacola" , 3.30);
-     bibite[1] = new Articolo("estathe" , 3.00);
-     bibite[2] = new Articolo("cocacola-zero" , 2.60);
-
-     
-
-
-
-
     public static void main(String[] args) {
 
+        scanner input = new Scanner(System.in);
 
-    gfu}
+        Articolo[] panini = {
+        new Articolo("hamburger",5.50),
+        new Articolo("toast",5.50),
+        new Articolo("ham",5.50)
+        };
+        Articolo[] bibite = {
+        new Articolo("acqua",5.50),
+        new Articolo("coca cola",5.5),
+        new Articolo("tè",5.50)
+        };
+        int scelta = -1;
+        while(scelta!=0)
+        {
+            System.out.println("\n===== MENU PRINCIPALE =====");
+            System.out.println("1 Aggiungi Panino");
+            System.out.println("2 Aggiungi Bibita");
+            System.out.println("3 Mostra Totale e Paga");
+            System.out.println("4 Elenca Articoli per Prezzo (Bubble Sort)");
+            System.out.println("0 Esci");
+            System.out.print("Scelta: ");
+            scelta=input.nextInt();
+            if(scelta == 1)
+            {
+                System.out.println("Scegli un panino");
+
+            }
+            else if(scelta == 2)
+            {
+
+            }
+            else if(scelta == 3)
+
+
+        }
+
+
+
+
+
+
 
     }
 
