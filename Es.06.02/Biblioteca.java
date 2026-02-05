da759577765ca9ea9b9b12abee3a2053363d8c92
Di Santo Andrea <andrea.disanto.08@marconigorgonzola.it>
2026-02-05 11:10:06 +0100

    agg.

---
+++ Es.06.02/Biblioteca.java
@@ -0,0 +1,11 @@
+package Es.06.02;
+
+public class Biblioteca {
+    private String CodiceUnivoco;
+    private String autore;
+    
+    public Biblioteca(String CodiceUnivoco,String autore){
+        this.CodiceUnivoco= CodiceUnivoco;
+        this.autore = autore;
+    }    
+}
---
+++ Es.06.02/Libro.java
@@ -0,0 +1,6 @@
+package Es.06.02;
+
+public class Libro {
+    
+    
+}
